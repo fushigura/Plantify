51a78bfeb0ceaecd208f02c28b2d152b81ed8c19
fushigura <hanzo343323@gmail.com>
2026-02-06 20:45:17 +0200

    Add header and start develop main

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,34 @@
+import "./Header.scss"
+import { useState } from "react"
+
 export default function Header(){
+        const [isNavActive, setIsNavActivve] = useState<number | null>(null)
+
     return(
         <>
-        
+        <header className=" flex justify-between">
+            <div className=" flex">
+                <img src="./img/logo.svg" alt="Logo Icon" />
+                <div className="logo ml-2 text-2xl">Plantify</div>
+            </div>
+            <ul  className="nav flex">
+                <li onClick={() => {setIsNavActivve(0)}} className={isNavActive === 0 ? "active" : ""}>Home</li>
+                <li onClick={() => {setIsNavActivve(1)}} className={isNavActive === 1 ? "active" : ""}>About</li>
+                <li onClick={() => {setIsNavActivve(2)}} className={isNavActive === 2 ? "active" : ""}>Contact</li>
+                <li onClick={() => {setIsNavActivve(3)}} className={isNavActive === 3 ? "active" : ""}>Plants</li>
+            </ul>
+            <ul className="profile flex">
+                <li>
+                    <img src="./img/search-icon.svg" alt="Search Icon" />
+                </li>
+                <li>
+                    <img src="./img/cart-icon.svg" alt="Cart icon" />
+                </li>
+                <li>
+                    <img src="./img/profile-icon.svg" alt="Profile Icon" />
+                </li>
+            </ul>
+        </header>
         </>
     )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,23 @@
+import Header from "../../components/Header/Header"
+import "./HomePage.scss"
+
 export default function HomePage(){
     return(
         <>
-        <h1>hello</h1>
+        <Header/>
+        <main>
+            <section className="hero-section">
+                <div>
+                    <h1>Bring Nature Into Your Home</h1>
+                    <p>Discover our collection of beautiful indoor plants that will 
+                    transform your space into a green paradise.</p>
+                    <div>
+                        <button>Shop Now</button>
+                        <button>Learn More</button>
+                    </div>
+                </div>
+            </section>
+        </main>
         </>
     )
 }
